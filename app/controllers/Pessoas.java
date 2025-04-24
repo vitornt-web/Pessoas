bab1b2cbc0a418b9409a2e078fc8cdacd4921ceb
vitornt-web <vitornt.profissional@gmail.com>
2025-04-24 13:44:31 -0300

    adicionando

---
+++ app/controllers/Pessoas.java
@@ -0,0 +1,11 @@
+package controllers;
+
+import models.Pessoa;
+import play.mvc.Controller;
+
+public class Pessoas extends Controller {
+  
+	public static void detalhar(Pessoa pessoa){
+		render();
+	}
+}
