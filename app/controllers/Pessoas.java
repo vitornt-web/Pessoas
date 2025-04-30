0a56641ef838f31cd5264a3f806afe4ca9b3aa01
vitornt-web <vitornt.profissional@gmail.com>
2025-04-30 10:59:02 -0300

    Exercicio 4 - completo

--- app/controllers/Pessoas.java
+++ app/controllers/Pessoas.java
@@ -4,8 +4,24 @@ import models.Pessoa;
 import play.mvc.Controller;
 
 public class Pessoas extends Controller {
+	
+	public static void form() {
+		render(); 
+	}
   
 	public static void detalhar(Pessoa pes){
-		render();
+		render(pes);
+	}
+	
+	public static void salvar(Pessoa pes) {
+		if(pes.nome != null) {
+			pes.nome = pes.nome.toUpperCase();
+		}
+		if(pes.email != null) {
+			pes.email = pes.email.toLowerCase();
+		}
+		
+		pes.save();
+		detalhar(pes);
 	}
 }
--- app/models/Pessoa.java
+++ app/models/Pessoa.java
@@ -1,6 +1,11 @@
 package models;
 
-public class Pessoa {
+import javax.persistence.Entity;
+
+import play.db.jpa.Model;
+
+@Entity
+public class Pessoa extends Model{
   
 	public String nome;
 	public String email;
